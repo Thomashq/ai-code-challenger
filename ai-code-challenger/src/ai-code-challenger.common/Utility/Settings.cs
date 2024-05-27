3b192d10f62459e24dc620249d44f15f7fb66c4e
Thomashq <thomashenriqq@gmail.com>
2024-05-27 19:39:48 -0300

    remoção da chave para evitar bug

--- ai-code-challenger/src/ai-code-challenger.common/Utility/Settings.cs
+++ ai-code-challenger/src/ai-code-challenger.common/Utility/Settings.cs
@@ -2,5 +2,5 @@ namespace ai_code_challenger.common;
 
 public static class Settings
 {
-    public static string Secret = "ec6ae50c7a44436790dab095e26cd91b";
+    public static string Secret = "";
 }
